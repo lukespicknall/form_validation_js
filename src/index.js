7d11b255ea756772b9b9f540cc3914078e644983
Luke Spicknall <lukejspicknall@gmail.com>
2024-03-13 22:15:17 -0400

    create showError function for each input that checks validty and throws error and changes style if needed. its working for email and zip and halfway for passwords.

--- src/index.js
+++ src/index.js
@@ -1,84 +1,169 @@
-import "./style.css";
+import './style.css';
 
 // create DOM elements and thier attributes
-const content = document.getElementById("content");
+const content = document.getElementById('content');
 
-const display = document.createElement("div");
-display.id = "display";
+const display = document.createElement('div');
+display.id = 'display';
 
-const formCard = document.createElement("div");
-formCard.id = "form-card";
+const formCard = document.createElement('div');
+formCard.id = 'form-card';
 
-const form = document.createElement("form");
-form.id = "form";
+const form = document.createElement('form');
+form.id = 'form';
 form.noValidate = true;
 
-const email = document.createElement("input");
-email.id = "email";
-email.type = "email";
-email.name = "email";
+const email = document.createElement('input');
+email.id = 'email';
+email.type = 'email';
+email.name = 'email';
 email.required = true;
-email.placeholder = 'Enter your email address'
-email.setCustomValidity("");
-email.classList.add("def-input");
-const emailLabel = document.createElement("label");
-emailLabel.setAttribute("for", "email");
-emailLabel.classList.add("labels");
-emailLabel.textContent = "Email";
-const emailError = document.createElement("span");
-emailError.classList.add("error");
+email.placeholder = 'Enter your email address';
+email.setCustomValidity('');
+email.classList.add('def-input');
+const emailLabel = document.createElement('label');
+emailLabel.setAttribute('for', 'email');
+emailLabel.classList.add('labels');
+emailLabel.textContent = 'Email';
+const emailError = document.createElement('span');
+emailError.classList.add('error');
 
-email.oninput = () => {
-  if (email.validity.valid) {
-    emailError.textContent = "";
-  } else if (!email.validity.valid) {
-    emailError.textContent = "i dont thoink so!";
+const showEmailError = () => {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = 'You need to enter an email address.';
+    email.className = 'notValid';
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = 'Entered value needs to be an email address.';
+    email.className = 'notValid';
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    email.className = 'notValid';
   }
 };
 
-const country = document.createElement("input");
-country.id = "country";
-country.type = " select";
+email.oninput = () => {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    email.className = 'isValid';
+  } else if (!email.validity.valid) {
+    // emailError.textContent = 'i dont thoink so!';
+    showEmailError();
+    email.className = 'notValid';
+  }
+};
+
+const country = document.createElement('input');
+country.id = 'country';
+country.type = ' select';
 country.required = true;
-country.classList.add("def-input");
-const countryLabel = document.createElement("label");
-countryLabel.classList.add("labels");
-countryLabel.textContent = "Country";
-const countryError = document.createElement("span");
-countryError.classList.add("error");
+country.classList.add('def-input');
+const countryLabel = document.createElement('label');
+countryLabel.classList.add('labels');
+countryLabel.textContent = 'Country';
+const countryError = document.createElement('span');
+countryError.classList.add('error');
 
-const zipCode = document.createElement("input");
-zipCode.id = "zip-code";
-zipCode.type = "text";
+const zipCode = document.createElement('input');
+zipCode.id = 'zip-code';
+zipCode.type = 'text';
 zipCode.required = true;
-zipCode.classList.add("def-input");
-const zipCodeLabel = document.createElement("label");
-zipCodeLabel.classList.add("labels");
-zipCodeLabel.textContent = "Zip Code";
-const zipError = document.createElement("span");
-zipError.classList.add("error");
+zipCode.pattern = '[0-9]{5}';
+zipCode.maxLength = 5;
+zipCode.placeholder = 'Enter a 5 digit zipcode';
+zipCode.classList.add('def-input');
+const zipCodeLabel = document.createElement('label');
+zipCodeLabel.classList.add('labels');
+zipCodeLabel.textContent = 'Zip Code';
+const zipCodeError = document.createElement('span');
+zipCodeError.classList.add('error');
 
-const password = document.createElement("input");
-password.id = "password";
-password.type = "password";
+const showZipCodeError = () => {
+  if (zipCode.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    zipCodeError.textContent = 'Enter a 5 digit Zip Code.';
+    zipCode.className = 'notValid';
+  } else if (zipCode.validity.patternMismatch) {
+    // If the field doesn't contain an zipCode address,
+    // display the following error message.
+    zipCodeError.textContent = 'Enter a 5 digit Zip Code.';
+    zipCode.className = 'notValid';
+  }
+};
+
+zipCode.oninput = () => {
+  if (zipCode.validity.valid) {
+    zipCodeError.textContent = '';
+    zipCode.className = 'isValid';
+  } else if (!zipCode.validity.valid) {
+    showZipCodeError();
+    zipCode.className = 'notValid';
+  }
+};
+
+const password = document.createElement('input');
+password.id = 'password';
+password.type = 'password';
 password.required = true;
-password.classList.add("def-input");
-const passwordLabel = document.createElement("label");
-passwordLabel.classList.add("labels");
-passwordLabel.textContent = "Password";
-const passwordError = document.createElement("span");
-passwordError.classList.add("error");
+password.pattern = '(?=.*d)(?=.*[a-zA-Z]).{6,}';
+password.minLength = 6;
+password.classList.add('def-input');
+const passwordLabel = document.createElement('label');
+passwordLabel.classList.add('labels');
+passwordLabel.textContent = 'Password';
+const passwordError = document.createElement('span');
+passwordError.classList.add('error');
 
-const passwordConfirm = document.createElement("input");
-passwordConfirm.id = "password-confirm";
-passwordConfirm.type = "password";
+const showPasswordError = () => {
+  if (password.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    passwordError.textContent = 'Enter a password';
+    password.className = 'notValid';
+  } else if (password.validity.patternMismatch && password.validity.tooShort) {
+    // If the field doesn't contain an password address,
+    // display the following error message.
+    passwordError.textContent = '6 characters long with a number and a letter';
+    password.className = 'notValid';
+  } else if (password.validity.tooShort) {
+    // If the field doesn't contain an password address,
+    // display the following error message.
+    passwordError.textContent = 'Must be 6 charecters long ';
+    password.className = 'notValid';
+  } else if (password.validity.patternMismatch) {
+    // If the field doesn't contain an password address,
+    // display the following error message.
+    passwordError.textContent = 'Must contain a number and a letter';
+    password.className = 'notValid';
+  }
+};
+
+password.oninput = () => {
+  if (password.validity.valid) {
+    passwordError.textContent = "";
+    password.className = "isValid";
+  } else if (!password.validity.valid) {
+    showPasswordError();
+    password.className = "notValid";
+  }
+};
+
+const passwordConfirm = document.createElement('input');
+passwordConfirm.id = 'password-confirm';
+passwordConfirm.type = 'password';
 passwordConfirm.required = true;
-passwordConfirm.classList.add("def-input");
-const passwordConfirmLabel = document.createElement("label");
-passwordConfirmLabel.classList.add("labels");
-passwordConfirmLabel.textContent = "Confirm Password";
-const passwordConfirmError = document.createElement("span");
-passwordConfirmError.classList.add("error");
+passwordConfirm.classList.add('def-input');
+const passwordConfirmLabel = document.createElement('label');
+passwordConfirmLabel.classList.add('labels');
+passwordConfirmLabel.textContent = 'Confirm Password';
+const passwordConfirmError = document.createElement('span');
+passwordConfirmError.classList.add('error');
 
 const inputs = [
   emailLabel,
@@ -89,7 +174,7 @@ const inputs = [
   countryError,
   zipCodeLabel,
   zipCode,
-  zipError,
+  zipCodeError,
   passwordLabel,
   password,
   passwordError,
@@ -100,8 +185,8 @@ const inputs = [
 
 // put each pair of label, input, and error span into a formField div for styling
 for (let i = 0; i < inputs.length; i += 3) {
-  const formField = document.createElement("div");
-  formField.classList.add("form-field");
+  const formField = document.createElement('div');
+  formField.classList.add('form-field');
   const grabber = i + 1;
   const grabber2 = i + 2;
   formField.appendChild(inputs[i]);
@@ -113,12 +198,28 @@ for (let i = 0; i < inputs.length; i += 3) {
   form.appendChild(formField);
 }
 
-const submitBox = document.createElement("div");
-const submitBtn = document.createElement("button");
-submitBtn.id = "submit-button";
-submitBtn.textContent = "Submit";
+const submitBox = document.createElement('div');
+const submitBtn = document.createElement('button');
+submitBtn.id = 'submit-button';
+submitBtn.textContent = 'Submit';
 submitBox.appendChild(submitBtn);
 form.appendChild(submitBox);
+
+submitBtn.onclick = (e) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showEmailError();
+    // Then we prevent the form from being sent by canceling the event
+    e.preventDefault();
+  }
+  if (!zipCode.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showZipCodeError();
+    // Then we prevent the form from being sent by canceling the event
+    e.preventDefault();
+  }
+};
 
 formCard.appendChild(form);
 
@@ -127,25 +228,25 @@ display.append(formCard);
 content.appendChild(display);
 
 // js style that ought to be replaced within style.css
-submitBtn.style.width = "150px";
+submitBtn.style.width = '150px';
 
-submitBox.style.display = "flex";
-submitBox.style.justifyContent = "center";
-submitBox.style.marginTop = "5px";
+submitBox.style.display = 'flex';
+submitBox.style.justifyContent = 'center';
+submitBox.style.marginTop = '5px';
 
-form.style.display = "flex";
-form.style.justifyContent = "center";
-form.style.flexDirection = "column";
-form.style.width = "275px";
+form.style.display = 'flex';
+form.style.justifyContent = 'center';
+form.style.flexDirection = 'column';
+form.style.width = '275px';
 
-formCard.style.display = "flex";
-formCard.style.justifyContent = "center";
-formCard.style.alignItems = "center";
-formCard.style.backgroundColor = "whitesmoke";
-formCard.style.padding = "40px";
+formCard.style.display = 'flex';
+formCard.style.justifyContent = 'center';
+formCard.style.alignItems = 'center';
+formCard.style.backgroundColor = 'whitesmoke';
+formCard.style.padding = '40px';
 
-display.style.display = "flex";
-display.style.width = "100%";
-display.style.height = "100vh";
-display.style.justifyContent = "center";
-display.style.alignItems = "center";
+display.style.display = 'flex';
+display.style.width = '100%';
+display.style.height = '100vh';
+display.style.justifyContent = 'center';
+display.style.alignItems = 'center';
